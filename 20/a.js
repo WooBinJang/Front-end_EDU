3d61080bbede82fb39941c954cfec5fca5e82eaf
Jang Oo Bin <jwb449730@naver.com>
2022-01-17 17:47:22 +0900

    변수, 호이스팅,this

--- 20/a.js
+++
@@ -1,7 +0,0 @@
-var userId = "Kim";
-console.log(userId); // return : "Kim"
-console.log(window.userId); // return : "Kim"
-//var 키워드로 만든 전역변수는 window에도 보관된다
-
-window.userAge = 60; // 전역변수 만들기
-console.log(window.userAge); // return : 60
