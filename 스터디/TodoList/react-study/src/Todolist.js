b25673079901fc73e59e38e39818f66115cd48de
Jang Oo Bin <jwb449730@naver.com>
2022-01-23 22:49:13 +0900

    react todolist 기능 추가2

--- 스터디/TodoList/react-study/src/Todolist.js
+++ 스터디/TodoList/react-study/src/Todolist.js
@@ -1,23 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Todolist.css";
 
 function Todolist() {
-  let [todoList, todoListUpdate] = useState(["오늘의 할 일을 입력하세요"]);
+  // let [todoList, todoListUpdate] = useState(["오늘의 할 일을 입력하세요"]);
+  let [todoList, todoListUpdate] = useState([
+    {
+      text: "오늘의 할 일을 입력하세요",
+      id: 0,
+      state: 0,
+      must: false,
+    },
+  ]);
   let [todoInput, todoInputUpdate] = useState("");
   let [todoStrong, todoStrongUpdate] = useState(false);
+
   let todo = (e) => {
+    if (todoInput === "") {
+      alert("오늘의 할 일을 입력하세요");
+      return false;
+    }
+
     let arrList = [...todoList];
     for (let x of arrList) {
-      if (x === "오늘의 할 일을 입력하세요") {
+      if (x.text === "오늘의 할 일을 입력하세요") {
         arrList.pop();
+      } else if (x.text === todoInput) {
+        alert("중복되는 목록입니다");
+        return false;
       }
     }
-    arrList.push(todoInput);
+    arrList.push({
+      text: todoInput,
+      id: arrList.length,
+      state: 0,
+      must: todoStrong,
+    });
+
     todoListUpdate(arrList);
 
     todoInputUpdate(""); // 입력 후 빈값 만들기
   };
-
+  console.log(todoList);
   // 글자 입력 시 입력값을 저장하는 이벤트
   let todoInputChg = (e) => {
     todoInputUpdate(e.target.value);
@@ -28,6 +51,43 @@ function Todolist() {
     todoStrongUpdate(!todoStrong);
   };
 
+  // todolist 삭제
+  let tododelete = (e) => {
+    let li = e.target.parentElement;
+    li.remove();
+    // 문제점 :  todoList 요소가 계속 남아 있음
+    // todoList.splice(li.ld, 1)
+  };
+
+  // li 영역 클릭 시 3단계의 변화를 준다. (3번을 초과하면 다시 초기상태로) 진행상태 표시
+  function todoStateChg(e) {
+    let txt = e.target.innerText.substr(2, e.target.innerText.length - 5);
+    let countid = 0;
+
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i].text === txt) {
+        countid = todoList[i].id;
+        todoList[i].state += 1;
+      }
+    }
+
+    if (todoList[countid].state > 2) {
+      todoList[countid].state = 0;
+    }
+
+    if (todoList[countid].state === 0) {
+      document.querySelectorAll(".list-item")[countid].firstChild.innerText =
+        "🔴";
+    } else if (todoList[countid].state === 1) {
+      document.querySelectorAll(".list-item")[countid].firstChild.innerText =
+        "🟠";
+    } else if (todoList[countid].state === 2) {
+      document.querySelectorAll(".list-item")[countid].firstChild.innerText =
+        "🟢";
+    }
+  }
+
+  // console.log(todoList);
   return (
     <div className="todolist-app">
       <div className="note">
@@ -35,13 +95,18 @@ function Todolist() {
         <ul className="todo">
           {todoList.map((todoItem, index) => {
             return (
-              <li className="list-item" key={index} id={index}>
-                {todoStrong ? (
-                  <span style={{ color: "red" }}> {todoItem}</span>
-                ) : (
-                  <span> {todoItem}</span>
-                )}
-                <button type="button" className="list-item-del-btn">
+              <li
+                className={todoItem.must ? "list-item on" : "list-item"}
+                key={index}
+                onClick={todoStateChg}
+              >
+                <span>🔴</span>
+                {todoItem.text}
+                <button
+                  type="button"
+                  className="list-item-del-btn"
+                  onClick={tododelete}
+                >
                   삭제
                 </button>
               </li>
